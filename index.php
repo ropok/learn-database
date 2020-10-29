37b1c1dc9a9c2efe88a5ca5ecdd64542d4f5bc05
ropok <jalerse@gmail.com>
2020-10-29 22:37:15 +0700

    learning php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!-- <?php
+// $var=4;
+// $var3=6;
+// $var2="<html>
+// <head></head>
+// <body><h1>Hello from php</h1></body>
+// </html>";
+
+// echo $var+$var3;
+// echo $var2;
+
+$newvar = "this is new new";
+$check=5;
+
+if($check==5){
+    echo $newvar;
+}
+
+while($check<7){
+    echo $check."<br>";
+    $check++;
+}
+
+?> -->
